d3fb4f33837b8d98e5f5272a03cadf5da638c784
Eliana Ocampo <49297799+ElianaOcampo@users.noreply.github.com>
2019-05-31 17:56:53 -0300

    Add files via upload

---
+++ Dominio/Vouchers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Vouchers
+    {
+        public int Id { get; set;}
+        public string CodigoVoucher { get; set; }
+        public bool Estado { get; set; }
+        public DateTime FechaRegistro;
+        public Clientes cliente;
+        public Productos producto;
+    }
+}
